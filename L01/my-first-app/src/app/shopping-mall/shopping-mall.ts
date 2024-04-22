577a6be99c493f5f874953ed60067627a8626374
Karthik <56579449+karthik9728@users.noreply.github.com>
2024-04-22 22:55:01 +0530

    Types of Component Selector

--- L01/my-first-app/src/app/shopping-mall/shopping-mall.ts
+++ L01/my-first-app/src/app/shopping-mall/shopping-mall.ts
@@ -1,7 +1,12 @@
 import { Component } from '@angular/core';
 
 @Component({
+  //Element
   selector: 'app-shopping-mall',
+  //Attribute
+  //selector: '[app-shopping-mall]',
+  //Css Class
+  //selector: '.app-shopping-mall',
   templateUrl: './shopping-mall.html',
   styleUrl: './shopping-mall.css',
 })
